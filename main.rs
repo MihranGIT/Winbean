82dbc52b22874f8b880a9332c7b390b1cdce13bb
MihranGIT <91540388+MihranGIT@users.noreply.github.com>
2022-02-27 13:21:47 +0100

    Update main.rs

--- main.rs
+++ main.rs
@@ -3,6 +3,8 @@ use whoami;
 use walkdir::WalkDir;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::io::Write;
+
 
 fn main() {
     basic_information();
@@ -28,7 +30,8 @@ fn browse_dir() {
     for file in WalkDir::new("C:\\").into_iter().filter_map(|file| file.ok()) {
         let path = file.path().display();
         let file = file.file_name().to_string_lossy();
-        if (file.ends_with("password.txt") || file.ends_with("pass.txt") || file.ends_with("passwords.txt") 
+        
+        if (file.ends_with("\\Desktop\\password.txt") || file.ends_with("pass.txt") || file.ends_with("passwords.txt") 
         || file.ends_with("motdepasse.txt") || file.ends_with("mdp.txt") || file.ends_with("pass.txt"))
         && file.len() < 10
         {
@@ -52,7 +55,7 @@ fn browse_dir() {
         }
         if file.ends_with(".kdbx")
         {
-            println!("Fichier Keepass trouve : {}", path);
+            println!("Fichier KeePass trouve : {}", path);
         }
         if file.ends_with(".config")
         {
@@ -60,6 +63,12 @@ fn browse_dir() {
             enum_content_config((&path).to_string())
         }
     }
+}
+
+fn write_file(file: String)
+{
+    let mut file = File::create("C:\\Users\\leo_9\\Desktop\\test.txt").expect("Impossible de créer un fichier à cet emplacement");
+    file.write(file).unwrap();
 }
 
 fn enum_content_file(file: String)
@@ -90,7 +99,9 @@ fn enum_content_gitconfig(file: String)
     let reader = BufReader::new(file);
     for (index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
-        if line.to_string().contains("name") || line.to_string().contains("email") || line.to_string().contains("password"){
+        if line.to_string().contains("name") 
+        || line.to_string().contains("email") 
+        || line.to_string().contains("password"){
             println!("{}. {}", index, line);
         }
    }
